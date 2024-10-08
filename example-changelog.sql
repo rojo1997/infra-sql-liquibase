9cc505878a2068d82ee0c30cba33ad9f3c8d6abd
Ernesto Martínez del Pino <ernestomar1997@hotmail.com>
2024-10-08 03:14:58 +0200

    test cicd

--- example-changelog.sql
+++ example-changelog.sql
@@ -2,3 +2,6 @@
 
 --changeset your.name:1
 CREATE TABLE test_table (test_id INT NOT NULL, test_column INT, PRIMARY KEY (test_id))
+
+--changeset your.name:2
+ALTER TABLE test_table ADD COLUMN test_column2 INT
